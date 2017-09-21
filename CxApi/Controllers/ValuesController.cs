80cb65f12188fb9fe68206f664342bcc6fda55e2
Vladimir Zorin <vladimir.zorin@computas.com>
2017-09-21 17:43:26 +0200

    made hello api

---
+++ CxApi/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CxApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        // GET api/hello
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello Who?";
+        }
+
+        // GET api/hello/name
+        [HttpGet("{name}")]
+        public string Get(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
